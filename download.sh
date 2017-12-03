209c389239fd72db2f8986f94fd3027fb9503746
Adam Harley <adam.harley@ryerson.ca>
2017-12-03 00:29:18 -0500

    got list of unique labels

--- download.sh
+++ download.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-rsync -vaP -e "ssh" matrix.ml.cmu.edu:~/ranseq/100_*.txt .
+# rsync -vaP -e "ssh" matrix.ml.cmu.edu:~/ranseq/100_*.txt .
+rsync -vaP -e "ssh" matrix.ml.cmu.edu:~/ranseq/*uni*.txt .
