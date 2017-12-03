f0cd5fc1438bcd1822c3002c4f1f76c50eb29f86
Adam Harley <adam.harley@ryerson.ca>
2017-12-03 01:23:44 -0500

    downloaded profile images

--- download.sh
+++ download.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
 # rsync -vaP -e "ssh" matrix.ml.cmu.edu:~/ranseq/100_*.txt .
-rsync -vaP -e "ssh" matrix.ml.cmu.edu:~/ranseq/*uni*.txt .
+# rsync -vaP -e "ssh" matrix.ml.cmu.edu:~/ranseq/*uni*.txt .
+rsync -vaP -e "ssh" matrix.ml.cmu.edu:~/ranseq/profile_images .
