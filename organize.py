6237bcfbae3eef023a13a5a8e96e0a484a470034
Adam Harley <adam.harley@ryerson.ca>
2017-12-03 00:52:04 -0500

    updated organizer script to count the number of examples of each label

--- organize.py
+++ organize.py
@@ -11,12 +11,8 @@ def mkdir(d):
 def shorten(name):
     return ''.join(e for e in name if e.isalnum())
 
-names=[]
-ages=[]
 
 unique_file = 'labels_unique.txt';
-covars_file = 'covars_all/part00';
-labels_file = 'labels_all/part00';
 G = 20499
 n = 160
 N = n*n
@@ -25,44 +21,66 @@ N = n*n
 root_dir = 'profile_images'
 mkdir(root_dir)
 
-with open(covars_file,'r') as f:
-    reader = csv.reader(f,delimiter='\t')
-    genes = list(reader)
-    nGenes = len(genes)
-    print 'found %d genes' % nGenes
-with open(labels_file,'r') as f:
-    reader = csv.reader(f,delimiter='\t')
-    labels = list(reader)
-    nLabels = len(labels)
-    print 'found %d labels' % nLabels
-assert(nGenes==nLabels)
 with open(unique_file,'r') as f:
     reader = csv.reader(f,delimiter='\t')
     uniques = list(reader)
-    nUniques = len(uniques)
-    print 'found %d unique labels' % nUniques
-    names = []
-    for ind in range(1,nUniques): # start at 1 to skip header
+    nCats = len(uniques)
+    print 'found %d unique labels' % nCats
+    cats = []
+    for ind in range(1,nCats): # start at 1 to skip header
         uniq = shorten(uniques[ind][0])
-        names.append(uniq)
+        cats.append(uniq)
 
+print cats        
 maxPngs = 100
-for ind in range(nUniques):
-    name = names[ind]
-    # out_dir = '%s/%s' % (root_dir, name)
-    
-    # find all profiles with this name
-    for ind in range(1,nGenes): # start at 1 to skip header
-        label = shorten(labels[ind][0])
-        count = 0
-        if label==name:
-            # if count < maxPngs
-            gene = genes[ind]
-            gene = np.asarray(gene).astype(np.float32)
-            # print 'gene %d; type %s' % (ind, label)
-            count += 1
-    print 'found %d gene profiles with the name %s' % (count, name)
-    raw_input()
+nParts = 43 # hardcode this for now
+
+cat_counts = np.zeros(nCats)
+print cat_counts
+# for ind in range(nUniques):
+#     name = names[ind]
+#     # out_dir = '%s/%s' % (root_dir, name)
+
+for part in range(nParts):
+    print 'reading part %d/%d' % (part, nParts-1)
+    # covars_file = 'covars_all/part%02d' % part
+    # with open(covars_file,'r') as f:
+    #     reader = csv.reader(f,delimiter='\t')
+    #     genes = list(reader)
+    #     nGenes = len(genes)
+    #     print 'found %d genes' % nGenes
+    labels_file = 'labels_all/part%02d' % part
+    with open(labels_file,'r') as f:
+        reader = csv.reader(f,delimiter='\t')
+        labels = list(reader)
+        nLabels = len(labels)
+        print 'found %d labels' % nLabels
+    # assert(nGenes==nLabels)
+    nGenes = nLabels
+
+    if part==0:
+        start=1 # start at 1 to skip header
+    else:
+        start=0
+    for gene_ind in range(start,nGenes):
+        label = shorten(labels[gene_ind][0])
+        cat_ind = cats.index(label)
+        print 'part %02d/%d; gene %d/%d; label %s; cat index %d' % (part, nParts-1,
+                                                                    gene_ind, nGenes-1,
+                                                                    label,
+                                                                    cat_ind)
+        cat_counts[cat_ind] += 1
+        print cat_counts
+    print '-'*100
+    # raw_input()
+
+        # if label==name:
+        #     # if count < maxPngs
+        #     gene = genes[ind]
+        #     gene = np.asarray(gene).astype(np.float32)
+        #     # print 'gene %d; type %s' % (ind, label)
+        #     count += 1
+    # print 'found %d gene profiles with the name %s' % (count, name)
         
 # for ind in range(1,nGenes): # start at 1 to skip header
 #     gene = genes[ind]
