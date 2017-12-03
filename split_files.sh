3c3b44dfb1ee755ab83b5f5d6945daca4be37545
Adam Harley <adam.harley@ryerson.ca>
2017-12-03 00:29:52 -0500

    made file splitter, to read the data more easily

---
+++ split_files.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+FN=train_covariates.tsv
+# split -l 1000 -d --additional-suffix=.txt $FN covars_all/part
+split -l 500 -d $FN covars_all/part
+
+
+FN=train_observed_labels.tsv
+# split -l 1000 -d --additional-suffix=.txt $FN covars_all/part
+split -l 500 -d $FN labels_all/part
+
+
+FN=train_experiment_ids.tsv
+# split -l 1000 -d --additional-suffix=.txt $FN covars_all/part
+split -l 500 -d $FN ids_all/part
