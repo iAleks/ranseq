a17c1ed84a5a0c2a18d12c19566d3cec68c2562c
Adam Harley <adam.harley@ryerson.ca>
2017-12-03 21:25:24 -0500

    dealt with experiment ids

--- organize.py
+++ organize.py
@@ -20,37 +20,32 @@ with open(unique_file,'r') as f:
     nCats = len(uniques)
     print 'found %d unique labels' % nCats
     cats = []
-    for ind in range(1,nCats): # start at 1 to skip header
+    for ind in range(nCats):
         uniq = shorten(uniques[ind][0])
+        print uniques[ind]
+        print uniq
         cats.append(uniq)
 
 print cats        
-maxPngs = 100
 nParts = 43 # hardcode this for now
-
 cat_counts = np.zeros(nCats)
-print cat_counts
 
 for part in range(nParts):
     print 'reading part %d/%d' % (part, nParts-1)
     labels_file = 'labels_all/part%02d' % part
     with open(labels_file,'r') as f:
-        reader = csv.reader(f,delimiter='\t')
+        reader = csv.reader(f,delimiter='\n')
         labels = list(reader)
         nLabels = len(labels)
         print 'found %d labels' % nLabels
-    # assert(nGenes==nLabels)
-    nGenes = nLabels
-
     if part==0:
-        start=1 # start at 1 to skip header
-    else:
-        start=0
-    for gene_ind in range(start,nGenes):
-        label = shorten(labels[gene_ind][0])
+        labels = labels[1:]
+        nLabels = nLabels - 1
+    for label_ind in range(nLabels):
+        label = shorten(labels[label_ind][0])
         cat_ind = cats.index(label)
-        print 'part %02d/%d; gene %d/%d; label %s; cat index %d' % (part, nParts-1,
-                                                                    gene_ind, nGenes-1,
+        print 'part %02d/%d; label %d/%d; label %s; cat index %d' % (part, nParts-1,
+                                                                    label_ind, nLabels-1,
                                                                     label,
                                                                     cat_ind)
         cat_counts[cat_ind] += 1
--- visualize.py
+++ visualize.py
@@ -2,7 +2,6 @@ import csv
 import numpy as np
 import os.path
 from scipy.misc import imsave
-import matplotlib.pyplot as plt
 
 def mkdir(d):
     if not os.path.exists(d):
@@ -18,6 +17,14 @@ print cat_counts
 print cats
 nCats = len(cat_counts)
 
+with open("eids_unique.txt",'r') as f:
+    reader = csv.reader(f,delimiter='\n')
+    eids = list(reader)
+eids = [int(eid[0]) for eid in eids]
+nEids = len(eids)
+print nEids
+print eids
+
 G = 20499
 n = 160
 N = n*n
@@ -25,13 +32,14 @@ N = n*n
 
 root_dir = 'profile_images'
 mkdir(root_dir)
-maxPngs = 100
+minPngs = 0
+maxPngs = 50
 nParts = 43 # hardcode this for now
 
-# nProfiles = 5
-print cats.index('CL0000057fibroblast')
-
-png_counts = np.zeros(nCats)
+# png_counts = np.zeros([nEids,nCats])
+count = 0
+cat_map = -1*np.ones(nCats)
+cat_iterator = 0
 for part in range(nParts):
     print 'reading part %d/%d' % (part, nParts-1)
     covars_file = 'covars_all/part%02d' % part
@@ -39,13 +47,19 @@ for part in range(nParts):
         reader = csv.reader(f,delimiter='\t')
         genes = list(reader)
         nGenes = len(genes)
-        print 'found %d genes' % nGenes
+        # print 'found %d genes' % nGenes
     labels_file = 'labels_all/part%02d' % part
     with open(labels_file,'r') as f:
         reader = csv.reader(f,delimiter='\t')
         labels = list(reader)
         nLabels = len(labels)
-        print 'found %d labels' % nLabels
+        # print 'found %d labels' % nLabels
+    eids_file = 'ids_all/part%02d' % part
+    with open(eids_file,'r') as f:
+        reader = csv.reader(f,delimiter='\t')
+        eids = list(reader)
+        nEids = len(eids)
+        # print 'found %d eids' % nEids
     # assert(nGenes==nLabels)
     nGenes = nLabels
     if part==0:
@@ -55,19 +69,29 @@ for part in range(nParts):
     for gene_ind in range(start,nGenes):
         cat = shorten(labels[gene_ind][0])
         cat_ind = cats.index(cat)
-        print 'part %02d/%d; gene %d/%d; label %s; cat index %d' % (part, nParts-1,
-                                                                    gene_ind, nGenes-1,
-                                                                    cat,
-                                                                    cat_ind)
-        if cat_counts[cat_ind] > 100 and png_counts[cat_ind] < 100:
-            print 'found example %d of cat %d, %s' % (png_counts[cat_ind], cat_ind, cat)
-            gene = genes[gene_ind]
-            gene = np.asarray(gene).astype(np.float32)
-            gene = np.log(gene+1)
-            gene = np.pad(gene, (0, N-G), 'constant')
-            gene = np.reshape(gene, [n, n])
-            out_dir = '%s/%s' % (root_dir, cat)
-            mkdir(out_dir)
-            imsave('%s/%04d.png' % (out_dir, png_counts[cat_ind]), gene)
-            png_counts[cat_ind] += 1
+        eid = shorten(eids[gene_ind][0])
+        print 'part %02d/%d; gene %d/%d; cat %s; cat index %d; eid %s; nCats %d' % (part, nParts-1,
+                                                                                    gene_ind, nGenes-1,
+                                                                                    cat,
+                                                                                    cat_ind,
+                                                                                    eid,
+                                                                                    np.amax(cat_map)+1)
+        # if cat_counts[cat_ind] > minPngs and png_counts[int(eid),cat_ind] < maxPngs:
+        if cat_map[cat_ind]==-1:
+            cat_map[cat_ind] = cat_iterator
+            print 'adding category %d' % cat_iterator
+            cat_iterator += 1
+        cat_id = cat_map[cat_ind]
+        # print 'found example %d of cat %d, %s' % (png_counts[int(eid), cat_ind], cat_ind, cat)
+        gene = genes[gene_ind]
+        gene = np.asarray(gene).astype(np.float32)
+        gene = np.log(gene+1)
+        gene = np.pad(gene, (0, N-G), 'constant')
+        gene = np.reshape(gene, [n, n])
+        out_dir = '%s/%s/%04d' % (root_dir, eid, cat_id)
+        mkdir(out_dir)
+        imsave('%s/%06d.png' % (out_dir, count), gene)
+        # png_counts[int(eid), cat_ind] += 1
+        count += 1
     print '-'*100
+print 'total %d categories' % (np.amax(cat_map)+1)
