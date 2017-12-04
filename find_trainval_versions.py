c4d1f5dd4fae0a41b86d21e421ffd89ae16c6c2b
Adam Harley <adam.harley@ryerson.ca>
2017-12-04 16:26:11 -0500

    added better checks of category coverage

--- find_trainval_versions.py
+++ find_trainval_versions.py
@@ -17,15 +17,16 @@ nExperiments = len(e_ids)
 
 version = 0
 print 'searching for version %d' % version
-while version < 10:
+while version < 5:
     random.shuffle(e_ids)
-    e_ids_train = e_ids[:int(np.floor(nExperiments*0.8))]
-    e_ids_val = e_ids[int(np.floor(nExperiments*0.8)):]
+    e_ids_train = e_ids[:int(np.floor(nExperiments*0.5))]
+    e_ids_val = e_ids[int(np.floor(nExperiments*0.5)):]
     e_ids_train.sort()
     e_ids_val.sort()
     # print e_ids_train
     # print e_ids_val
-    cat_coverage = np.zeros(nCats, np.int64)
+    cat_coverage_t = np.zeros(nCats, np.int64)
+    cat_coverage_v = np.zeros(nCats, np.int64)
     for e_id in e_ids_train:
         e_id_dir = '%s/%s' % (image_dir, e_id)
         c_ids = os.listdir(e_id_dir)
@@ -36,22 +37,43 @@ while version < 10:
             if os.path.isdir(cat_dir):
                 ims = os.listdir(cat_dir)
                 nIms = len(ims)
-                cat_coverage[cat_id] += nIms
-    # print cat_coverage
-    cat_cover = np.sum(cat_coverage > 0)
-    # print 'this covers %d/%d cats' % (cat_cover, nCats)
-    if cat_cover==nCats:
+                cat_coverage_t[cat_id] += nIms
+    for e_id in e_ids_val:
+        e_id_dir = '%s/%s' % (image_dir, e_id)
+        c_ids = os.listdir(e_id_dir)
+        for c_id in c_ids:
+            cat_dir = '%s/%s' % (e_id_dir, c_id)
+            cat_id = int(c_id)
+            # print 'looking at %s; cat_id = %d' % (cat_dir, cat_id)
+            if os.path.isdir(cat_dir):
+                ims = os.listdir(cat_dir)
+                nIms = len(ims)
+                cat_coverage_v[cat_id] += nIms
+    cat_cover_t = np.sum(cat_coverage_t > 0)
+    cat_cover_v = np.sum(cat_coverage_v > 0)
+    cat_cover_both = np.sum((cat_coverage_v>0) == (cat_coverage_t>0))
+    # print ((cat_coverage_t>0).astype(int) - (cat_coverage_v>0).astype(int))
+    # if np.float32(cat_cover_t)/nCats > 0.68 and np.float32(cat_cover_v)/nCats > 0.68:
+    if np.float32(cat_cover_both)/nCats > 0.4: 
         print 'got a valid version!'
+        print 'this covers %d/%d cats in train (%.2f), and %d/%d cats in val (%.2f), and %d/%d cats in both (%.2f)' % (
+            cat_cover_t, nCats, np.float32(cat_cover_t)/nCats,
+            cat_cover_v, nCats, np.float32(cat_cover_v)/nCats,
+            cat_cover_both, nCats, np.float32(cat_cover_both)/nCats)
+        print ((cat_coverage_t>0).astype(int) - (cat_coverage_v>0).astype(int))
         print e_ids_train
+        print e_ids_val
         exists = 0
         for v in range(version):
             otherv = np.load('%s/e_ids_train_%d.npy' % (data_dir, v))
             eq = otherv==e_ids_train
             if np.sum(eq)==len(eq):
                 exists = 1
+                print '!'*100
                 print 'this seems to be an exact copy of version %d' % v
         if not exists:
             print 'this version does not yet exist! saving!'
+            np.save('%s/cat_cover_both_%d.npy' % (data_dir, version), cat_cover_both)
             np.save('%s/e_ids_train_%d.npy' % (data_dir, version), e_ids_train)
             np.save('%s/e_ids_val_%d.npy' % (data_dir, version), e_ids_val)
             version += 1
