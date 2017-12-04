30209e9926de2451b3750ac9ac96115f3ca7d354
Adam Harley <adam.harley@ryerson.ca>
2017-12-03 21:28:18 -0500

    deleted files that do not belong in the repo

--- download.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-# rsync -vaP -e "ssh" matrix.ml.cmu.edu:~/ranseq/100_*.txt .
-# rsync -vaP -e "ssh" matrix.ml.cmu.edu:~/ranseq/*uni*.txt .
-rsync -vaP -e "ssh" matrix.ml.cmu.edu:~/ranseq/profile_images .
--- upload.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-rsync -vaP -e "ssh" * matrix.ml.cmu.edu:~/ranseq/
