930245bea94efd52a06a90a79b769c2ed45db706
Adam Harley <adam.harley@ryerson.ca>
2017-12-04 16:26:50 -0500

    changed the writer to print floats instead of uint8 images

--- write_tfrs.py
+++ write_tfrs.py
@@ -1,8 +1,33 @@
+import csv
 from PIL import Image
 import numpy as np
 import tensorflow as tf
 import sys
 import os.path
+
+def mkdir(d):
+    if not os.path.exists(d):
+        os.makedirs(d)
+
+def shorten(name):
+    return ''.join(e for e in name if e.isalnum())
+
+data_dir = 'data'
+cat_counts = np.load('%s/cat_counts.npy' % data_dir)
+cats = np.load('%s/cats.npy' % data_dir).tolist()
+print cat_counts
+print cats
+nCats = len(cat_counts)
+
+with open("eids_unique.txt",'r') as f:
+    reader = csv.reader(f,delimiter='\n')
+    eids = list(reader)
+eids = [int(eid[0]) for eid in eids]
+nEids = len(eids)
+print nEids
+print eids
+
+G = 20499
 
 def _floats_feature(value):
     return tf.train.Feature(float_list=tf.train.FloatList(value=value))
@@ -16,45 +41,67 @@ def _int64_feature(value):
 def _int64s_feature(value):
     return tf.train.Feature(int64_list=tf.train.Int64List(value=value))
 
-out_dir = "data/tfrs"
+out_dir = "data/tfrs2"
 if not os.path.exists(out_dir):
     os.makedirs(out_dir)
-  
-image_dir = "profile_images"
-  
-e_ids = os.listdir(image_dir)
-for e_id in e_ids:
-    e_id_dir = '%s/%s' % (image_dir, e_id)
-    c_ids = os.listdir(e_id_dir)
-    for c_id in c_ids:
-        cat_id = int(c_id)
-        cat_dir = '%s/%s' % (e_id_dir, c_id)
-        if os.path.isdir(cat_dir):
-            ims = os.listdir(cat_dir)
-            nIms = len(ims)
-            im_id = 0
-            for im in ims:
-                out_file = "%s/%s_%04d_%04d.tfrecord" % (out_dir,e_id,cat_id,im_id)
-                if os.path.isfile(out_file):
-                    sys.stdout.write(':')
-                else:
-                    im_path = '%s/%s' % (cat_dir, im)
-                    image = np.array(Image.open(im_path))
-                    height = int(image.shape[0])
-                    width = int(image.shape[1])
-                    image_raw = image.tostring()
-                    compress = tf.python_io.TFRecordOptions(
-                        compression_type=tf.python_io.TFRecordCompressionType.GZIP)
-                    writer = tf.python_io.TFRecordWriter(out_file, options=compress)
-                    example = tf.train.Example(features=tf.train.Features(feature={
-                        'height': _int64_feature(height),
-                        'width': _int64_feature(width),
-                        'image_raw': _bytes_feature(image_raw),
-                        'cat_id': _int64_feature(cat_id),
-                    }))
-                    writer.write(example.SerializeToString())
-                    writer.close()
-                    sys.stdout.write('.')
-                sys.stdout.flush()
-                im_id += 1
+
+nParts = 43 # hardcode this for now
+im_id = 0
+cat_map = -1*np.ones(nCats, np.int64)
+cat_iterator = 0
+for part in range(nParts):
+    print 'reading part %d/%d' % (part, nParts-1)
+    covars_file = 'covars_all/part%02d' % part
+    with open(covars_file,'r') as f:
+        reader = csv.reader(f,delimiter='\t')
+        genes = list(reader)
+        nGenes = len(genes)
+        # print 'found %d genes' % nGenes
+    labels_file = 'labels_all/part%02d' % part
+    with open(labels_file,'r') as f:
+        reader = csv.reader(f,delimiter='\t')
+        labels = list(reader)
+        nLabels = len(labels)
+        # print 'found %d labels' % nLabels
+    eids_file = 'ids_all/part%02d' % part
+    with open(eids_file,'r') as f:
+        reader = csv.reader(f,delimiter='\t')
+        eids = list(reader)
+        nEids = len(eids)
+        # print 'found %d eids' % nEids
+    # assert(nGenes==nLabels)
+    nGenes = nLabels
+    if part==0:
+        start=1 # start at 1 to skip header
+    else:
+        start=0
+    for gene_ind in range(start,nGenes):
+        cat = shorten(labels[gene_ind][0])
+        cat_ind = cats.index(cat)
+        eid = shorten(eids[gene_ind][0])
+        if cat_map[cat_ind]==-1:
+            cat_map[cat_ind] = cat_iterator
+            print 'adding category %d' % cat_iterator
+            cat_iterator += 1
+        cat_id = cat_map[cat_ind]
+        
+        out_file = "%s/%s_%04d_%08d.tfrecord" % (out_dir,eid,cat_id,im_id)
+        im_id += 1
+        if os.path.isfile(out_file):
+            sys.stdout.write(':')
+        else:
+            gene = genes[gene_ind]
+            gene = np.asarray(gene).astype(np.float32)
+            gene_raw = gene.tostring()
+            compress = tf.python_io.TFRecordOptions(
+                compression_type=tf.python_io.TFRecordCompressionType.GZIP)
+            writer = tf.python_io.TFRecordWriter(out_file, options=compress)
+            example = tf.train.Example(features=tf.train.Features(feature={
+                'gene_raw': _bytes_feature(gene_raw),
+                'cat_id': _int64_feature(cat_id),
+            }))
+            writer.write(example.SerializeToString())
+            writer.close()
+            sys.stdout.write('.')
+        sys.stdout.flush()
 print 'done'      
